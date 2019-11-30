edd1ad346d5bac4824c33952cb9d1e7c05646b14
mszczepaniuk <asd2ws@gmail.com>
2019-11-30 21:53:53 +0100

    Created core models

--- Models/Meme.cs
+++ Models/Meme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace memiarzeEu.Models
 {
     public class Meme
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageLink { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
--- Models/XdPoint.cs
+++ Models/XdPoint.cs
@@ -7,6 +7,10 @@ namespace memiarzeEu.Models
 {
     public class XdPoint
     {
-
+        public int Id { get; set; }
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public int MemeId { get; set; }
+        public Meme Meme { get; set; }
     }
 }
