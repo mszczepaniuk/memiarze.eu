395a4c9ca428ce68980ad14deb43cd28074f968a
mszczepaniuk <asd2ws@gmail.com>
2019-12-01 14:32:40 +0100

    Changed AddDbContext to AddDbContextPool

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace memiarzeEu
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContextPool<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddIdentity<ApplicationUser, IdentityRole>(options => {
