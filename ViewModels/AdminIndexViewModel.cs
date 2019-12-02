b5e6f798e44f99381f0bfb2a9261f31050dbaff6
mszczepaniuk <asd2ws@gmail.com>
2019-12-02 15:24:05 +0100

    Revert "Revert "Small changes in admin page""
    This reverts commit da4d0b8ad184484e383d3a04e6da537fda9b9a1d.

--- ViewModels/AdminIndexViewModel.cs
+++ ViewModels/AdminIndexViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using memiarzeEu.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace memiarzeEu.ViewModels
 {
     public class AdminIndexViewModel
     {
+        [Required]
         public string UserName { get; set; }
         public ICollection<ApplicationUser> Admins { get; set; }
     }
