a9cb4765ae3eb7e48c5e7d7cea64b9949e0c6d07
mszczepaniuk <asd2ws@gmail.com>
2019-12-02 15:22:57 +0100

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -104,5 +104,12 @@ namespace memiarzeEu.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult AwardXdIndex(ICollection<Meme> memes)
+        {
+
+            return View("Index");
+        }
     }
 }
